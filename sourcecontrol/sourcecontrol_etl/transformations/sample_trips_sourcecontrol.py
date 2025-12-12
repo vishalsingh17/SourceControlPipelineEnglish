b1c88ac72b1af6562325ccfe16bee7a612e22f8b
vishal170997@gmail.com <vishal170997@gmail.com>
2025-12-12 08:50:22 +0000

    second commit

--- sourcecontrol/sourcecontrol_etl/transformations/sample_trips_sourcecontrol.py
+++ sourcecontrol/sourcecontrol_etl/transformations/sample_trips_sourcecontrol.py
@@ -6,7 +6,6 @@ from pyspark.sql.functions import col
 # Edit the sample below or add new transformations
 # using "+ Add" in the file browser.
 
-
 @dp.table
 def sample_trips_sourcecontrol():
     return spark.read.table("samples.nyctaxi.trips")
